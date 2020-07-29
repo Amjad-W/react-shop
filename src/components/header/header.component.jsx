a0eabc6b0df30e418b354869ab6a5e096f52e2b4
Amjad W <amjad-shomali@hotmail.com>
2020-07-29 17:53:32 +0300

    Forgot to add Redux folder, added cart redux and show cart dropdown functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import { Link } from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assets/crown.svg';
 import { auth } from "../../firebase/firebase.utils";
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -16,16 +20,22 @@ const Header = ({currentUser}) => (
             <Link to='/about' className="option">ABOUT</Link>
             {
                 currentUser ?
-                <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>SIGN IN</Link>
+                    <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div>
+                    :
+                    <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        {
+            hidden ? null :
+                <CartDropdown />
+        }
     </div>
 );
 
-const mapStateToProps = (state) => ({
-   currentUser: state.user.currentUser 
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header);
